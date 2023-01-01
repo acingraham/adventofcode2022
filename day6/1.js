54d6b210229b44d8a6f4398ffedddf0b49b24643
Andrew Ingraham <acingraham@gmail.com>
2023-01-01 14:02:08 -0500

    Remove console.log

--- day6/1.js
+++ day6/1.js
@@ -5,7 +5,6 @@ var file = (0, fs_1.readFileSync)('./input.txt', 'utf-8');
 for (var i = 0; i < file.length; i++) {
     var str = file.slice(i, i + 4);
     var set = new Set(str.split(''));
-    console.log(set);
     if (set.size === 4) {
         console.log(i + 4);
         break;
